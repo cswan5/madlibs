bd6d9b6d678469edd0f8a6f4e00cd9f8865d0a12
Swanson, Christine <acswanso@nsf.gov>
2025-09-16 11:54:18 -0400

    update to bslib

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(bslib)
 
 generate_story <- function(noun, verb, adjective, adverb) {
   glue::glue(
@@ -9,19 +10,19 @@ generate_story <- function(noun, verb, adjective, adverb) {
   )
 }
 
-ui <- fluidPage(
-  titlePanel("Mad Libs Game"),
-  sidebarLayout(
+ui <- page_sidebar(
+  title="Mad Libs Game",
+  sidebar=sidebar(
     sidebarPanel(
       textInput("noun1", "Enter a noun:", ""),
       textInput("verb", "Enter a verb:", ""),
       textInput("adjective", "Enter an adjective:", ""),
       textInput("adverb", "Enter an adverb:", "")
-    ),
-    mainPanel(
-      h3("Your Mad Libs Story:"),
-      textOutput("story")
     )
+    ),
+    card(
+      card_header("Your Mad Libs Story"),
+      textOutput("story")
   )
 )
 
